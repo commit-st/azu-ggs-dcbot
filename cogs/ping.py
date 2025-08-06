25c796c4dc2d2ccfcfa49c52907e152afac6ef96
commit-st <rabbitlapinbunny@gmail.com>
2025-08-07 03:57:17 +0900

    Update and rename example.py to ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,12 +1,14 @@
+# cogs/ping.py
 from discord.ext import commands
 
-class Example(commands.Cog):
+class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="핑")
     async def ping(self, ctx):
+        """!핑 → 모두에게 퐁!"""
         await ctx.send("퐁! 🏓")
 
 async def setup(bot):
-    await bot.add_cog(Example(bot))
+    await bot.add_cog(Ping(bot))
